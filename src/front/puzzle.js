a9e6eb13592b9af47af0de0f7d0de1ba90254f74
Julien Douet <j.douet@alpha8.pro>
2021-02-03 07:50:50 +0100

    added functionnal puzzle view

--- src/front/puzzle.js
+++ src/front/puzzle.js
@@ -1,5 +1,17 @@
 $(document).ready(() => {
 
+    $("#logs").click(() => {
+        console.table(personalComputerStructureData);
+    });
+
+    // Init
+
+
+    let outputData = [
+
+    ];
+
+
     const monitorHeader = {
         title : "Monitor",
         labelList : ["13\'","15\'","15\'6","21\'5","27\'"]
@@ -25,85 +37,165 @@ $(document).ready(() => {
         [false,false,false,false,false]
     ]; 
 
+    const generateRowHeaderTable = (headerData) => {
+        let generatedTable = "<table class=\"top-table\"><tbody><tr><td colspan=\"5\">"+headerData.title+"</td></tr>";
+        headerData.labelList.forEach((label) => {
+            generatedTable += "<td class='stickLeft'><div class=\"vertical\">"+label+"</div></td>";
+        })
+        generatedTable += "</tr></tbody></table>";
+        return generatedTable;
+    }
+
+    const generateColumnHeaderTable = (headerData) => {
+        let generatedTable = "<table class=\"side-table\"><tbody><tr><td rowspan=\"5\"><div class=\"vertical\">"+headerData.title+"</div></td><td>"+headerData.labelList[0]+"</td></tr>";
+        headerData.labelList.forEach((label,index) => {
+            if(index>0)
+                generatedTable += "<tr><td>"+label+"</td></tr>";
+        })
+        generatedTable += "</tbody></table>";
+        return generatedTable;
+    }
+
+    const generateInnerFiveByFiveTable = (parentIndex, childIndex) => {
+        let generatedTable = "<table class=\"middle-table\"><tbody>";
+        personalComputerStructureData[parentIndex][childIndex].centerDatatable.valueList.forEach((row, rowIndex)=> {
+            generatedTable += "<tr>";
+            row.forEach((cell, cellIndex) => {
+                generatedTable += "<td colspan=\"1\" tableRowIndex='"+parentIndex+"' tableColumnIndex='"+childIndex+"' cellRowIndex='"+rowIndex+"' cellColumnIndex='"+cellIndex+"'  style='text-align: center;'></td>";
+            })
+            generatedTable += "</tr>";
+        });
+        generatedTable += "</tbody></table>";
+        return generatedTable;
+    }
+
     let personalComputerStructureData = [
-        {
-            leftPane : monitorHeader,
-            upperPane : processorHeader,
-            centerDatatable : {
-                valueList : generateFiveByFiveEmptyTab()
-            }
-        },
-        {
-            leftPane : monitorHeader,
-            upperPane : hardDiskHeader,
-            centerDatatable : {
-                valueList : generateFiveByFiveEmptyTab()
-            }
-        },
-        {
-            leftPane : monitorHeader,
-            upperPane : priceHeader,
-            centerDatatable : {
-                valueList : generateFiveByFiveEmptyTab()
-            }
-        },
-        {
-            leftPane : priceHeader,
-            upperPane : processorHeader,
-            centerDatatable : {
-                valueList : generateFiveByFiveEmptyTab()
-            }
-        },
-        {
-            leftPane : priceHeader,
-            upperPane : hardDiskHeader,
-            centerDatatable : {
-                valueList : generateFiveByFiveEmptyTab()
-            }
-        },
-        {
-            leftPane : hardDiskHeader,
-            upperPane : processorHeader,
-            centerDatatable : {
-                valueList : generateFiveByFiveEmptyTab()
-            }
-        },
+        [
+            {
+                leftPane : monitorHeader,
+                upperPane : processorHeader,
+                centerDatatable : {
+                    valueList : generateFiveByFiveEmptyTab()
+                }
+            },
+            {
+                leftPane : monitorHeader,
+                upperPane : hardDiskHeader,
+                centerDatatable : {
+                    valueList : generateFiveByFiveEmptyTab()
+                }
+            },
+            {
+                leftPane : monitorHeader,
+                upperPane : priceHeader,
+                centerDatatable : {
+                    valueList : generateFiveByFiveEmptyTab()
+                }
+            },
+        ],
+        [
+            {
+                leftPane : priceHeader,
+                upperPane : processorHeader,
+                centerDatatable : {
+                    valueList : generateFiveByFiveEmptyTab()
+                }
+            },
+            {
+                leftPane : priceHeader,
+                upperPane : hardDiskHeader,
+                centerDatatable : {
+                    valueList : generateFiveByFiveEmptyTab()
+                }
+            },
+        ],
+        [
+            {
+                leftPane : hardDiskHeader,
+                upperPane : processorHeader,
+                centerDatatable : {
+                    valueList : generateFiveByFiveEmptyTab()
+                }
+            },
+        ]
     ]
 
 
     $("#personalComputerPuzzle").append(
         "<table>"+
-            "<tbody>"+
-                "<tr id='firstRow'>"+
-                    "<td class='1'></td>"+
-                    "<td class='2'></td>"+
-                    "<td class='3'></td>"+
-                    "<td class='4'></td>"+
+            "<tbody class='fixedCell'>"+
+                "<tr>"+
+                    "<td></td>"+
+                    "<td></td>"+
+                    "<td></td>"+
+                    "<td></td>"+
                 "</tr>"+
-                "<tr id='secondRow'>"+
-                    "<td class='1'></td>"+
-                    "<td class='2'></td>"+
-                    "<td class='3'></td>"+
-                    "<td class='4'></td>"+
+                "<tr>"+
+                    "<td></td>"+
+                    "<td></td>"+
+                    "<td></td>"+
+                    "<td></td>"+
                 "</tr>"+
-                "<tr id='thirdRow'>"+
-                    "<td class='1'></td>"+
-                    "<td class='2'></td>"+
-                    "<td class='3'></td>"+
+                "<tr>"+
+                    "<td></td>"+
+                    "<td></td>"+
+                    "<td></td>"+
                 "</tr>"+
-                "<tr id='fourthRow'>"+
-                    "<td class='1'></td>"+
-                    "<td class='2'></td>"+
+                "<tr>"+
+                    "<td></td>"+
+                    "<td></td>"+
                 "</tr>"+
             "</tbody>"+
         "</table>"
     );
+
     $("div#personalComputerPuzzle > table > tbody > tr").each(function (parentIndex) {
         $(this).children().each(function (childIndex) {
-            console.log("chained index : "+parentIndex+"-"+childIndex);
-            $(this).append($(this).attr('class'));
+            if(parentIndex == 0) { // Upper row
+                if(childIndex == 0) { // First Cell -> To be empty
+                    $(this).css("border","0")
+                } else {
+                    //console.log(personalComputerStructureData[0][childIndex-1].upperPane);
+                    $(this).append(generateRowHeaderTable(personalComputerStructureData[0][childIndex-1].upperPane));
+                }
+            } else if(childIndex == 0) { // Left Column excepting upper left one
+                $(this).append(generateColumnHeaderTable(personalComputerStructureData[parentIndex-1][0].leftPane));
+            } else { // inner Table
+                $(this).append(generateInnerFiveByFiveTable(parentIndex-1,childIndex-1));
+            }
         });
     });
     
+
+    const correct = '<i class="fas fa-check green"></i>';
+    const wrong = '<i class="fas fa-times red"></i>';
+    // Data Processing
+
+    $('td[tableRowIndex]').click(function () {
+        const tableRowIndex = $(this).attr("tableRowIndex");
+        const tableColumnIndex = $(this).attr("tableColumnIndex");
+        const cellRowIndex = $(this).attr("cellRowIndex");
+        const cellColumnIndex =  $(this).attr("cellColumnIndex");
+        console.log(tableRowIndex+tableColumnIndex+cellRowIndex+cellColumnIndex);
+        if(!$(this).html()) {
+            $(this).html(wrong);
+        } else if($(this).html() == wrong){
+            $("td[tableRowIndex='"+tableRowIndex+"'][tableColumnIndex='"+tableColumnIndex+"'][cellRowIndex='"+cellRowIndex+"']").each(function (parentIndex) {
+                $(this).html(wrong);
+            });
+            $("td[tableRowIndex='"+tableRowIndex+"'][tableColumnIndex='"+tableColumnIndex+"'][cellColumnIndex='"+cellColumnIndex+"']").each(function (parentIndex) {
+                $(this).html(wrong);
+            });
+            $(this).html(correct);
+        } else {
+            $("td[tableRowIndex='"+tableRowIndex+"'][tableColumnIndex='"+tableColumnIndex+"'][cellRowIndex='"+cellRowIndex+"']").each(function (parentIndex) {
+                $(this).html("");
+            });
+            $("td[tableRowIndex='"+tableRowIndex+"'][tableColumnIndex='"+tableColumnIndex+"'][cellColumnIndex='"+cellColumnIndex+"']").each(function (parentIndex) {
+                $(this).html("");
+            });
+            $(this).html("");
+        }
+    });
 
 });
